15966d0e6bd8fe023ae7d8c9e6f730453be532ff
unknown <alexis.autotte@gmail.com>
2021-01-20 07:35:40 -0800

    4th commit 1- Configuring google and facebook Authentication

--- app/src/main/java/com/example/nofapmainactivity/SettingsActivity.java
+++ app/src/main/java/com/example/nofapmainactivity/SettingsActivity.java
@@ -1,14 +1,341 @@
 package com.example.nofapmainactivity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.squareup.picasso.Picasso;
+
 
 public class SettingsActivity extends AppCompatActivity {
-
+    private SignInButton signInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private  String TAGGoogle = "MainActivity";
+    private FirebaseAuth mAuth;
+    private Button btnSignOut;
+    private int RC_SIGN_IN = 1;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+    private CallbackManager mCallbackManager;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private TextView textViewUser;
+    private ImageView mLogo;
+    private LoginButton loginButton;
+    private AccessTokenTracker accessTokenTracker;
+    private static final String TAG = "FacebookAuthentication";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.IconProfileId)
+                {
+                    OpenActivity(ProfileActivity.class);
+                }
+
+                else if (id == R.id.IconSettingId)
+                {
+                    OpenActivity(SettingsActivity.class);
+                }
+
+                else if (id == R.id.IconTimerId)
+                {
+                    OpenActivity(TimerActivity.class);
+                }
+                return true;
+            }
+        });
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        textViewUser = findViewById(R.id.text_user);
+        mLogo = findViewById(R.id.image_logo);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setReadPermissions("email", "public_profile");
+
+        mCallbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSuccess" + loginResult);
+                handleFacebookToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "onError" + error);
+            }
+        });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null)
+                {
+                    updateUI(user, "Facebook");
+                }
+                else {
+                    updateUI(null, "Facebook");
+                }
+            }
+        };
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (currentAccessToken == null)
+                {
+                    mFirebaseAuth.signOut();
+                }
+            }
+        };
+
+        signInButton = findViewById(R.id.sign_in_button);
+        mAuth = FirebaseAuth.getInstance();
+        btnSignOut = findViewById(R.id.sign_out_button);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGoogleSignInClient.signOut();
+                Toast.makeText(SettingsActivity.this,"You are Logged Out",Toast.LENGTH_SHORT).show();
+                btnSignOut.setVisibility(View.INVISIBLE);
+            }
+        });
     }
+
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data, String method) {
+        if (method == "Facebook")
+        {
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        if (method == "Google")
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+            if(requestCode == RC_SIGN_IN){
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                handleSignInResult(task);
+            }
+        }
+
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask){
+        try{
+
+            GoogleSignInAccount acc = completedTask.getResult(ApiException.class);
+            Toast.makeText(SettingsActivity.this,"Signed In Successfully",Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(acc);
+        }
+        catch (ApiException e){
+            Toast.makeText(SettingsActivity.this,"Sign In Failed",Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(null);
+        }
+    }
+
+    private void FirebaseGoogleAuth(GoogleSignInAccount acct) {
+        //check if the account is null
+        if (acct != null) {
+            AuthCredential authCredential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+            mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(SettingsActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user, "Google");
+                    } else {
+                        Toast.makeText(SettingsActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        updateUI(null, "Google");
+                    }
+                }
+            });
+        }
+        else{
+            Toast.makeText(SettingsActivity.this, "acc failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateUI(FirebaseUser fUser, String method){
+
+        if (method == "Facebook")
+        {
+            if (fUser != null)
+            {
+
+                if (fUser.getPhotoUrl() != null)
+                {
+                    String photoUrl = fUser.getPhotoUrl().toString();
+                    photoUrl = photoUrl + "?type=large";
+                    Picasso.get().load(photoUrl).into(mLogo);
+                }
+
+            } else {
+                textViewUser.setText("");
+                mLogo.setImageResource(R.drawable.com_facebook_button_icon);
+            }
+
+        }
+
+        if (method == "Google")
+        {
+            btnSignOut.setVisibility(View.VISIBLE);
+            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+            if(account !=  null){
+                String personName = account.getDisplayName();
+                String personGivenName = account.getGivenName();
+                String personFamilyName = account.getFamilyName();
+                String personEmail = account.getEmail();
+                String personId = account.getId();
+                Uri personPhoto = account.getPhotoUrl();
+
+                Toast.makeText(SettingsActivity.this,personName + personEmail ,Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
+
+    }
+
+    private void handleFacebookToken(AccessToken token)
+    {
+        Log.d(TAG, "handleFacebookToken" + token);
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mFirebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    Log.d(TAG, "sign in with credential: successful");
+                    FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                    updateUI(user, "Facebook");
+                } else
+                {
+                    Log.d(TAG, "sign in with credential: failure", task.getException());
+                    Toast.makeText(SettingsActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                    updateUI(null, "Facebook");
+                }
+            }
+        });
+    }
+
+
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (authStateListener != null)
+        {
+            mFirebaseAuth.removeAuthStateListener(authStateListener);
+        }
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    private void OpenActivity(Class<?> ActivityToOpen)
+    {
+        Intent intent = new Intent(getApplicationContext(), ActivityToOpen);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/nofapmainactivity/TimerActivity.java
+++ app/src/main/java/com/example/nofapmainactivity/TimerActivity.java
@@ -1,96 +1,197 @@
 package com.example.nofapmainactivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.nofapmainactivity.models.TimerModel;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class TimerActivity extends AppCompatActivity {
-    TimerModel timer;
-    TextView mSeconds, mMinutes, mHours, mDays, mMonths, mYears;
-    String timerSecond, timerMinutes, timerHours, timerDays, timerMonths, timerYears;
-    Button mResetButton;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+    TextView timerText;
+    Button stopStartButton;
+
+    Timer timer;
+    TimerTask timerTask;
+    Double time = 0.0;
+
+    boolean timerStarted = false;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
-        mSeconds = findViewById(R.id.TimerSecondsId);
-        mMinutes = findViewById(R.id.TimerMinutesId);
-        mHours = findViewById(R.id.TimerHoursId);
-        mDays = findViewById(R.id.TimerDaysId);
-        mMonths = findViewById(R.id.TimerMonthsId);
-        mYears = findViewById(R.id.TimerYearsId);
+         dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
 
-        initTimer();
-        AddTimerTime();
-        mResetButton = findViewById(R.id.ButtonResetId);
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
 
-        mResetButton.setOnClickListener(new View.OnClickListener()
-        {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                ResetTimer();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.IconProfileId)
+                {
+                    OpenActivity(ProfileActivity.class);
+                }
+
+                else if (id == R.id.IconSettingId)
+                {
+                    OpenActivity(SettingsActivity.class);
+                }
+
+                else if (id == R.id.IconTimerId)
+                {
+                    OpenActivity(TimerActivity.class);
+                }
+                return true;
             }
         });
+
+        timerText = (TextView) findViewById(R.id.timerText);
+        stopStartButton = (Button) findViewById(R.id.startStopButton);
+
+        timer = new Timer();
     }
 
-    private void ResetTimer()
-    {
-        timer = new TimerModel();
-        timerSecond = Integer.toString(timer.Seconds);
-        timerMinutes = Integer.toString(timer.Minutes);
-        timerHours = Integer.toString(timer.Hours);
-        timerDays = Integer.toString(timer.Days);
-        timerMonths = Integer.toString(timer.Months);
-        timerYears = Integer.toString(timer.Years);
-        mSeconds.setText("Seconds : \t\t" + timerSecond);
-        mMinutes.setText("Minutes : \t\t" + timerMinutes);
-        mHours.setText("Hours : \t\t" + timerHours);
-        mDays.setText("Days : \t\t" + timerDays);
-        mMonths.setText("Months : \t\t" + timerMonths);
-        mYears.setText("Years : \t\t" + timerYears);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
-    private void initTimer()
+    private void OpenActivity(Class<?> ActivityToOpen)
     {
-        timer = new TimerModel();
-        timerSecond = Integer.toString(timer.Seconds);
-        timerMinutes = Integer.toString(timer.Minutes);
-        timerHours = Integer.toString(timer.Hours);
-        timerDays = Integer.toString(timer.Days);
-        timerMonths = Integer.toString(timer.Months);
-        timerYears = Integer.toString(timer.Years);
-        mSeconds.setText("Seconds : \t\t" + timerSecond);
-        mMinutes.setText("Minutes : \t\t" + timerMinutes);
-        mHours.setText("Hours : \t\t" + timerHours);
-        mDays.setText("Days : \t\t" + timerDays);
-        mMonths.setText("Months : \t\t" + timerMonths);
-        mYears.setText("Years : \t\t" + timerYears);
+        Intent intent = new Intent(getApplicationContext(), ActivityToOpen);
+        startActivity(intent);
+    }
+
+
+
+
+
+
+
+    public void resetTapped(View view)
+    {
+        AlertDialog.Builder resetAlert = new AlertDialog.Builder(this);
+        resetAlert.setTitle("Reset Timer");
+        resetAlert.setMessage("Are you sure you want to reset the timer?");
+        resetAlert.setPositiveButton("Reset", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                if(timerTask != null)
+                {
+                    timerTask.cancel();
+                    setButtonUI("START", R.color.green);
+                    time = 0.0;
+                    timerStarted = false;
+                    timerText.setText(formatTime(0,0,0));
+
+                }
+            }
+        });
+
+        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                //do nothing
+            }
+        });
+
+        resetAlert.show();
 
     }
 
-    private void AddTimerTime()
+    public void startStopTapped(View view)
     {
-        timer.AddTime();
-        timerSecond = Integer.toString(timer.Seconds);
-        timerMinutes = Integer.toString(timer.Minutes);
-        timerHours = Integer.toString(timer.Hours);
-        timerDays = Integer.toString(timer.Days);
-        timerMonths = Integer.toString(timer.Months);
-        timerYears = Integer.toString(timer.Years);
-        mSeconds.setText("Seconds : \t\t" + timerSecond);
-        mMinutes.setText("Minutes : \t\t" + timerMinutes);
-        mHours.setText("Hours : \t\t" + timerHours);
-        mDays.setText("Days : \t\t" + timerDays);
-        mMonths.setText("Months : \t\t" + timerMonths);
-        mYears.setText("Years : \t\t" + timerYears);
+        if(timerStarted == false)
+        {
+            timerStarted = true;
+            setButtonUI("STOP", R.color.red);
+
+            startTimer();
+        }
+        else
+        {
+            timerStarted = false;
+            setButtonUI("START", R.color.green);
+
+            timerTask.cancel();
+        }
+    }
+
+    private void setButtonUI(String start, int color)
+    {
+        stopStartButton.setText(start);
+        stopStartButton.setTextColor(ContextCompat.getColor(this, color));
+    }
+
+    private void startTimer()
+    {
+        timerTask = new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        time++;
+                        timerText.setText(getTimerText());
+                    }
+                });
+            }
+
+        };
+        timer.scheduleAtFixedRate(timerTask, 0 ,1000);
+    }
+
+
+    private String getTimerText()
+    {
+        int rounded = (int) Math.round(time);
+
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = ((rounded % 86400) / 3600);
+
+        return formatTime(seconds, minutes, hours);
+    }
+
+    private String formatTime(int seconds, int minutes, int hours)
+    {
+        return String.format("%02d",hours) + " : " + String.format("%02d",minutes) + " : " + String.format("%02d",seconds);
     }
 }
